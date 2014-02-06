d45d67ea7851e2c40b5213229b3b6d37e487d864
Bulten <alexander.hermansson.85@gmail.com>
2014-02-06 20:04:56 +0100

    Create TCPClient.java

---
+++ TCPClient.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+class TCPClient{
+	public static void main(String argv[]) throws Exception{
+	
+		String input;
+		String modifiedInput;
+		
+		BufferedReader userInput = 
+			new BufferedReader(new InputStreamReader(System.in));
+			
+		Socket clientSocket = new Socket("localhost", 6789);
+		
+		DataOutputStream toServer = 
+			new DataOutputStream(clientSocket.getOutputStream());
+		
+		BufferedReader fromServer = 
+			new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+		input = fromUser.readLine();
+		
+		toServer.writeBytes(input + '\n');
+		
+		modifiedInput = fromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedInput);
+		clientSocket.close();
+	}
+}
