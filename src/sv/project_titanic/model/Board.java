0a347712b7f95e012a36bd62e47664f53a7bc742
Sebasthian Karlsson <sebka@student.chalmers.se>
2014-02-14 11:29:33 +0100

    Cleanup

--- src/sv/project_titanic/model/Board.java
+++ src/sv/project_titanic/model/Board.java
@@ -1,70 +1,53 @@
 package sv.project_titanic.model;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
 import sv.project_titanic.model.Ship;
 
-import java.util.Observable;
-
-public class Board extends Observable
-{
-	private int boardX, boardY;
+public class Board extends Observable {
+	private int boardX;
+	private int boardY;
 	private ArrayList<Ship> fleet;
-	//private boolean playerTurn;
 	public int[][] fieldStatus;
-	private ArrayList<Coordinate> coordList;
 	
-	public Board(int xdim, int ydim)
-	{
+	public Board(int xdim, int ydim) {
 		this.boardX = xdim;
 		this.boardY = ydim;
 		
 		fleet = new ArrayList<Ship>();
-		
 		fieldStatus = new int[boardX][boardY];
-
-		for(int i=0 ; i < boardX ; i++){
-			for(int j=0 ; j < boardY; j++){
-				fieldStatus[i][j] = 0;
-			}
-		}
 	}
 	
-	public void addShip(Ship ship)
-	{
+	public void addShip(Ship ship) {
 		fleet.add(ship);
 
-		for(Coordinate c : ship.getCoords())
-		{
+		for(Coordinate c : ship.getCoords()) {
 			setFieldStatus(c.getX(), c.getY(), 2);
 		}
 	}
 	
-	public void setFieldStatus(int x, int y, int status)
-	{
+	public void setFieldStatus(int x, int y, int status) {
 		fieldStatus[x][y] = status;
 		setChanged();
 		int[] message = {x, y, status};
 		notifyObservers(message);
 	}
 	
-	public int getXdim()
-	{
+	public int getXdim() {
 		return boardX;
 	}
 	
-	public int getYdim()
-	{
+	public int getYdim() {
 		return boardY;
 	}
 	
-	public int getFieldStatus(int x, int y)
-	{
+	public int getFieldStatus(int x, int y) {
 		return fieldStatus[x][y];
 	}
 
-	public ArrayList<Ship> getFleet()
-	{
+	public ArrayList<Ship> getFleet() {
 		return fleet;
 	}
 }
+
