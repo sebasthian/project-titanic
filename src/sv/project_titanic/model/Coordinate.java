d66a0d636ff4a5f76386407244978086760fddf5
Sebasthian Karlsson <sebka@student.chalmers.se>
2014-02-17 15:43:37 +0100

    Code Cleanup

--- src/sv/project_titanic/model/Coordinate.java
+++ src/sv/project_titanic/model/Coordinate.java
@@ -1,39 +1,33 @@
 package sv.project_titanic.model;
 
-public class Coordinate
-{
-	private int x, y;
-	
-	
-	public Coordinate(int x, int y)
-	{
+public class Coordinate {
+	private int x;
+	private int y;
+
+	public Coordinate(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	/*
-	 * Compares Coordinate object with another. 
+
+	/**
+	 * Compare this coordinate with another. 
 	 */
-	public boolean equals(Coordinate coord)
-	{
-		if(coord.getX() == x && (coord.getY() == y))
-		{
-			return true;
-		}
-		return false;
+	public boolean equals(Coordinate other) {
+		return this.x == other.x && this.y == other.y;
 	}
+
 	/*
 	 * Return X
 	 */
-	public int getX()
-	{
+	public int getX() {
 		return x;
 	}
+
 	/*
 	 * Return Y
 	 */
-	public int getY()
-	{
+	public int getY() {
 		return y;
 	}
-	
 }
+
