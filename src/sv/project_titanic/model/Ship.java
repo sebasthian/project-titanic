026be1f257f619cdcbb51e176844144c3692f579
Sebasthian Karlsson <sebka@student.chalmers.se>
2014-02-17 16:09:29 +0100

    Code Cleanup

--- src/sv/project_titanic/model/Ship.java
+++ src/sv/project_titanic/model/Ship.java
@@ -3,74 +3,42 @@ package sv.project_titanic.model;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Ship 
-{
+public class Ship {
 	private static ArrayList<Coordinate> coords;
+	
+	public Ship(ArrayList<Coordinate> coords){
+		this.coords = new ArrayList<>();
 
-	
-	public Ship(ArrayList<Coordinate> inputCoords){
-		coords = new ArrayList<Coordinate>();
-		addShip(inputCoords);
+		for(Coordinate c : coords)
+			this.coords.add(c);
 	}
-	
-	/**
-	 * Private method to add coords to arrayList.
-	 */
-	private void addShip(ArrayList<Coordinate> inputCoords)
-	{
-		for(Coordinate inputCoord : inputCoords)
-		{
-			coords.add(inputCoord);
-		}
-	}
-	public ArrayList<Coordinate> getCoords(){
+
+	public ArrayList<Coordinate> getCoords() {
 		return coords;
 	}
-	
 	
 	/**
 	 * Checks if coord is a hit on the ship.
 	 * Returns nothing but removes the corresponding Coordinate object if hit.
 	 */
-	public void shipHit(Coordinate coord)
-	{
-		Iterator<Coordinate> it = coords.iterator();
-		
-		while(it.hasNext()){
-			Coordinate shipCoord = it.next();
-			
-			if(shipCoord.equals(coord)){
-				it.remove();
-				return;
-			}
-		}
+	public void shipHit(Coordinate coord) {
+		coords.remove(coord);
 	}
 	
 	/**
 	 * Checks if coords contains any Coordinate objects.
 	 * @return True if no Coordinate objects exist in coords.
 	 */
-	public boolean noMoreShip()
-	{
-		if(coords.isEmpty()){
-			return true;
-		}
-		return false;
+	public boolean noMoreShip() {
+		return coords.isEmpty();
 	}
 	
 	/**
 	 * Returns True if coords contains coord.
 	 * @return True if the ship contains the coordinate.
 	 */
-	public boolean hasCoordinate(Coordinate coord)
-	{
-		for(Coordinate shipCoord : coords){
-			if(shipCoord.equals(coord)){
-				return true;
-			}
-		}
-		return false;
+	public boolean hasCoordinate(Coordinate coord) {
+		return coords.contains(coord);
 	}
 }
-
 
