fe93fbeba8836606ac5f1134e51303729adc982a
Sebasthian Karlsson <sebka@student.chalmers.se>
2014-02-25 14:25:05 +0100

    Add Javadoc

--- src/sv/project_titanic/model/Player.java
+++ src/sv/project_titanic/model/Player.java
@@ -1,16 +1,23 @@
 package sv.project_titanic.model;
 
+/**Class for handling player data.*/
 public class Player {
 	private String name;
 	
+	/**Create a new player.
+	 *
+	 * @param name the name of the Player.
+	 */
 	public Player(String name) {
 		this.name = name;
 	}
 
+	/**@param name a new name for this player.*/
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**@return the name of this player.*/
 	public String getName() {
 		return name;
 	}
